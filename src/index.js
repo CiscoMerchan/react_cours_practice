00ec7707a038f1083021ed4880daadd4ee873837
CiscoMerchan <franciscoeliasm@yahoo.com>
2023-04-23 20:33:03 +1000

    map() function exercices

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
 // import App from "../src/keeper/App";
-import App from "../src/props/App"
+// import App from "../src/props/App"
+import App from "../src/mappingPractice/App"
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
