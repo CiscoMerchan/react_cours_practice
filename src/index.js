a3c97d16aca581b589af84a8645852bdce4df7d4
CiscoMerchan <franciscoeliasm@yahoo.com>
2023-04-23 15:18:34 +1000

    Creation of a new Card componend and rendered using props

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
 // import App from "../src/keeper/App";
-import App from "../src/props/App";
+import App from "../src/props/App"
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
